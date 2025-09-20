8e09cafdad8218973216694a5761ffce3c758a64
AymanRagab2006 <aymanr2710@gmail.com>
2025-09-20 14:48:05 +0300

    Updated Parking System

--- car_entrance.py
+++ car_entrance.py
@@ -1,38 +1,55 @@
 import RPi.GPIO as GPIO
+from BlynkLib import Blynk
 import time
 
-trigger_pin = 6
-echo_pin = 5
-gate_servo = 12
-red_led = 21
+BlYNK_AUTH = "FrG6_tvF2KEY3Chu5THFkLYhLMZ8E_DK"
+
+blynk = Blynk(BlYNK_AUTH, server='blynk.cloud', port=80)
+
+trigger_pin_entrance = 15
+echo_pin_entrance = 14
+trigger_pin_exit = 24
+echo_pin_exit = 23
+gate_entarnce_servo = 18
+gate_exit_servo = 21
+"""red_led = 21
 yellow_led = 20
-green_led = 16
+green_led = 16"""
 
 available_slots = 9
 
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(trigger_pin, GPIO.OUT)
-GPIO.setup(echo_pin, GPIO.IN)
-GPIO.setup(gate_servo, GPIO.OUT)
-GPIO.setup(red_led, GPIO.OUT)
+GPIO.setup(trigger_pin_entrance, GPIO.OUT)
+GPIO.setup(echo_pin_entrance, GPIO.IN)
+GPIO.setup(trigger_pin_exit, GPIO.OUT)
+GPIO.setup(echo_pin_exit, GPIO.IN)
+GPIO.setup(gate_entarnce_servo, GPIO.OUT)
+GPIO.setup(gate_exit_servo, GPIO.OUT)
+
+"""GPIO.setup(red_led, GPIO.OUT)
 GPIO.setup(yellow_led, GPIO.OUT)
-GPIO.setup(green_led, GPIO.OUT)
+GPIO.setup(green_led, GPIO.OUT)"""
 
-gate = GPIO.PWM(gate_servo, 50)
+gate_entrance = GPIO.PWM(gate_entarnce_servo, 50)
+gate_exit = GPIO.PWM(gate_exit_servo, 50)
 
-GPIO.output(trigger_pin, GPIO.LOW)
+GPIO.output(trigger_pin_entrance, GPIO.LOW)
+GPIO.output(trigger_pin_exit, GPIO.LOW)
+"""GPIO.output(red_led, GPIO.LOW)
+GPIO.output(yellow_led, GPIO.LOW)
+GPIO.output(green_led, GPIO.LOW)"""
+
 time.sleep(2)
 
-
-def get_distance():
-    GPIO.output(trigger_pin, GPIO.HIGH)
+def get_distance(trig, echo):
+    GPIO.output(trig, GPIO.HIGH)
     time.sleep(0.00001)
-    GPIO.output(trigger_pin, GPIO.LOW)
+    GPIO.output(trig, GPIO.LOW)
 
-    while GPIO.input(echo_pin) == 0:
+    while GPIO.input(echo) == 0:
         start_pulse = time.time()
-    while GPIO.input(echo_pin) == 1:
+    while GPIO.input(echo) == 1:
         end_pulse = time.time()
 
     pulse_duration = end_pulse - start_pulse
@@ -40,32 +57,59 @@ def get_distance():
     return distance
 
 while True:
-    print("hello")
-    dist = get_distance()
-    print(dist)
-    if dist < 10 and available_slots > 0:
-        # TODO: open gate 
 
+    #print("hello")
+
+    dist_entrance = get_distance(trigger_pin_entrance, echo_pin_entrance)
+    print(dist_entrance)
+
+    dist_exit = get_distance(trigger_pin_exit, echo_pin_exit)
+    print(dist_exit)
+
+    if dist_entrance < 10 and available_slots > 0:
         print(available_slots)
         available_slots -= 1
-        gate.start(0)
-        gate.ChangeDutyCycle(7) 
-        print("Gate opened")
-        GPIO.output(yellow_led,GPIO.LOW)
+        gate_entrance.start(0)
+        gate_entrance.ChangeDutyCycle(7) 
+        print("Entrance Gate opened")
+        """GPIO.output(yellow_led,GPIO.LOW)
         GPIO.output(red_led, GPIO.LOW)
-        GPIO.output(green_led, GPIO.HIGH)
+        GPIO.output(green_led, GPIO.HIGH)"""
         time.sleep(2)
+        """GPIO.output(red_led, GPIO.LOW)
+        GPIO.output(green_led, GPIO.LOW)
+        GPIO.output(yellow_led,GPIO.HIGH)"""
+        blynk.virtual_write(0, available_slots)
+        lcd.write_text("Slots: " + str(available_slots), 1)
+
+        gate_entrance.start(0)
+        gate_entrance.ChangeDutyCycle(2)
+    
+
+
+    if dist_exit < 10:
+        print(available_slots)
+        available_slots += 1
+        gate_exit.start(0)
+        gate_exit.ChangeDutyCycle(7) 
+        print("Exit Gate opened")
+        """GPIO.output(yellow_led,GPIO.LOW)
         GPIO.output(red_led, GPIO.LOW)
+        GPIO.output(green_led, GPIO.HIGH)"""
+        time.sleep(2)
+        """GPIO.output(red_led, GPIO.LOW)
         GPIO.output(green_led, GPIO.LOW)
-        GPIO.output(yellow_led,GPIO.HIGH)
-    else:
-        gate.start(0)
-        gate.ChangeDutyCycle(2)
-        print("Gate closed")
-        GPIO.output(green_led, GPIO.LOW)
-        GPIO.output(yellow_led,GPIO.LOW)
-        GPIO.output(red_led, GPIO.HIGH)
+        GPIO.output(yellow_led,GPIO.HIGH)"""
+        blynk.virtual_write(0, available_slots)
+
+        lcd.write_text("Slots: " + str(available_slots), 1)
+
+        gate_exit.start(0)
+        gate_exit.ChangeDutyCycle(2)
+    
 
     
-    time.sleep(1)
+    blynk.run()
 
+    
+    time.sleep(0.1)
--- tent_control.py
+++ tent_control.py
@@ -1,9 +1,16 @@
 import RPi._GPIO as GPIO
+from BlynkLib import Blynk
 import adafruit_dht
 import board
 import time
 
+BlYNK_AUTH = "FrG6_tvF2KEY3Chu5THFkLYhLMZ8E_DK"
+
+blynk = Blynk(BlYNK_AUTH, server='blynk.cloud', port=80)
+
 tent_servo = 12
+
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(tent_servo, GPIO.OUT)
@@ -22,10 +29,13 @@ while True:
             dht.start(0)
             dht.ChangeDutyCycle(2)
 
+        blynk.virtual_write(1, temp_c)
+
     except RuntimeError as error:
         print(error.args[0])
         time.sleep(1)
     except Exception as error:
         dht.exit()
         raise error
-    time.sleep(1)
+    blynk.run()
+    time.sleep(10)
